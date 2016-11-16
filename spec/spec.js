415503e0e7ccf5b94b10099927286f2ffa77c7f9
Andrei Dmytruk <mallboroks@mail.ru>
2016-11-17 01:04:57 +0200

    Unit

--- spec/spec.js
+++ spec/spec.js
@@ -1,13 +1,29 @@
-const script = require('../js/script.js');
+const script = require('../js/script.js'),
+  d = require('../js/script.js');
+
+
 
 describe("array", function() {
   it("property in the array", function() {
 
     //prepqre
-      var result;
+    var result;
     //act
-      result = ['Христофор Колумб', '1492', 'Китайцы','Ирак', 'Куба'];
+    result = script;
+    console.log(result);
     //assert
     expect(result).toContain('Христофор Колумб', '1492', 'Китайцы','Ирак', 'Куба');
+  });
+
+
+  it("rray", function() {
+
+    //prepqre
+      var s;
+    //act
+      s = script.hiF(2);
+      console.log(s);
+    //assert
+    expect(s).toEqual('hi 2');
   });
 });
